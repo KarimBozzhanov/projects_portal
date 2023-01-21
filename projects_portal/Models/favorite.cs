6c1960e2a9dea2d02493a6982d6c584923d80688
KarimBozzhanov <59042013+KarimBozzhanov@users.noreply.github.com>
2023-01-21 16:16:49 +0600

    new commit files

---
+++ projects_portal/Models/favorite.cs
@@ -0,0 +1,10 @@
+﻿namespace projects_portal.Models
+{
+    public class favorite
+    {
+        public int Id { get; set; }
+        public int userId { get; set; }
+        public string userNameFavorite { get; set; }
+        public int postFavoriteId { get; set; }
+    }
+}
