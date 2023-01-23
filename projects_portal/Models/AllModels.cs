eda0c3375ef9a7f4a6aedd4c793824dca86c5bcb
KarimBozzhanov <59042013+KarimBozzhanov@users.noreply.github.com>
2023-01-23 22:09:52 +0600

    Changes

---
+++ projects_portal/Models/AllModels.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace projects_portal.Models
+{
+    public class AllModels
+    {
+        public IEnumerable<Projects> projects { get; set; }
+        public IEnumerable<User> users { get; set; }
+        public IEnumerable<favorite> favorite { get; set; }
+    }
+}
---
+++ projects_portal/Models/Projects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace projects_portal.Models
+{
+    public class Projects
+    {
+        public int Id { get; set; }
+        public string NameOfProject { get; set; }
+        public string Name { get; set; }
+        public DateTime TimeOfCreating { get; set; }
+        public string Group { get; set; }
+        public string Description { get; set; }
+        public string PresentationName { get; set; }
+        public string PresentationPath { get; set; }
+        public string apkFileName { get; set; }
+        public string apkFilePath { get; set; }
+        public string urlGit { get; set; }
+        public string siteUrl { get; set; }
+        public string ImageName { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
